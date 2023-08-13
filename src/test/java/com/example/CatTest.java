ddc70c70f81bb09bdd7255d045f0da49f5aa3e94
Dmitry Zhegalov <dmitryzhee@gmail.com>
2023-08-13 10:25:30 +0300

    Added cat tests

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -1,5 +1,19 @@
 package com.example;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CatTest {
+
+
+  Feline feline = new Feline();
+  Cat cat = new Cat(feline);
+
+  @Test
+  public void getSoundSuccess () {
+    Assert.assertEquals("Мяу", cat.getSound());
+  }
+
+
 
 }
