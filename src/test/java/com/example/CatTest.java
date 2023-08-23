eef8721df41978e276f2b0053d3efd71defcaf26
Dmitry Zhegalov <dmitryzhee@gmail.com>
2023-08-23 17:29:50 +0300

    Changed CatTest

--- src/test/java/com/example/CatTest.java
+++ src/test/java/com/example/CatTest.java
@@ -4,13 +4,14 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CatTest {
-    @Spy
+    @Mock
     Feline feline;
 
     Cat cat;
