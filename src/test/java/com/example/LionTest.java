ba658038ab2fe7156376eac03880466e74174b21
Dmitry Zhegalov <dmitryzhee@gmail.com>
2023-08-24 12:14:20 +0300

    Changed LionTest

--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -4,30 +4,30 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LionTest {
-  @Spy
-  IFeline feline = new Feline();
+  @Mock
+  IFeline feline;
 
-  Lion lion;
-
-  @Before
-  public void setUp() {
-   lion = new Lion(feline);
-  }
 
     @Test
-  public void getKittenSuccess() {
-    Assert.assertTrue(lion.getKittens() == 1 );
+  public void getKittenSuccess() throws Exception {
+    Lion lion = new Lion(feline, "Самка");
+    lion.getKittens();
+    Mockito.verify(feline, Mockito.times(1)).getKittens();
   }
 
   @Test
   public void lionGetFoodSuccess() throws Exception {
+    Lion lion = new Lion(feline, "Самец");
     lion.getFood();
     Mockito.verify(feline, Mockito.times(1)).getFood("Хищник");
   }
+
+
 }
