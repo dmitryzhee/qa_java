b5df9b744662111dfd5052acd5ad91a6a5825f92
Dmitry Zhegalov <dmitryzhee@gmail.com>
2023-08-24 12:36:05 +0300

    Updated FelineTest

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -21,22 +21,20 @@ public class FelineTest {
 
   @Test
   public void getFamilyFeline() {
-    Assert.assertEquals("Кошачьи", feline.getFamily());
+    Feline familyFeline = new Feline();
+    Assert.assertEquals("Кошачьи", familyFeline.getFamily());
   }
 
   @Test
   public void getKittenSuccess() {
-    Assert.assertTrue(feline.getKittens() == 1 );  }
-
+    feline.getKittens();
+    Mockito.verify(feline, Mockito.times(1)).getKittens();}
 
 
   @Test
   public void getKittensSuccess() {
-    Assert.assertTrue(feline.getKittens(3) == 3);  }
-
-
-
-
+    feline.getKittens(3);
+    Mockito.verify(feline, Mockito.times(1)).getKittens(3);}
 
 }
 
